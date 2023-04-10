7e1f46b3ddba1ed18c6e1e44144904091e46077a
Adin Rama Ariyanto Putra <rp00038@gmail.com>
2023-04-10 10:20:40 +0700

    Upload tugas ArrayList

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+class Mahasiswa {
+  private String nama;
+  private String jurusan;
+  private boolean status; // true=Menikah ; false=Lajang
+
+  // Tambahkan constructor disini ...
+  Mahasiswa(String nama, String jurusan, boolean status) {
+    this.nama = nama;
+    this.jurusan = jurusan;
+    this.status = status;
+  }
+
+  // Tambahkan method setter & getter disini ...
+}
